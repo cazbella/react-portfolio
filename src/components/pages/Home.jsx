03845dd8877cbe21c77fdaf4a45c29d971e15a15
CarolineBellaLane <carolinelane@live.co.uk>
2024-01-08 10:57:03 +0000

    tries to fix homepage issues

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import Jumbotron from "../jumbotron/Jumbotron";
-import Header from "../layout/Header";
-import Card from "../cards/Card";
 
 
 function Home() {
   return (
     <div>
         <Jumbotron />
-        {/* <Card /> */}
     </div>
   );
 }
