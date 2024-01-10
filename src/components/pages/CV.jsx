0396ea110daaa20259d132a9d64f83591381355f
CarolineBellaLane <carolinelane@live.co.uk>
2024-01-10 11:28:59 +0000

    further cv styling

--- src/components/pages/CV.jsx
+++ src/components/pages/CV.jsx
@@ -10,16 +10,14 @@ function CV() {
   return (
     <div>
       <Header title="CV" />
-      <div className="container-fluid mb-4 cv-container">
-        <div className="container-fluid mb-4" id="cv-container">
-          <p>Please find a link below to my CV in PDF format and images of my CV.</p>
-          <div className="cv-images mx-auto">
+      <div className="container-fluid mb-4" id="cv-container">
+        <p>Please find a link below to my CV in PDF format and images of my CV.</p>
+        <div className="cv-images mx-auto">
           <a href={cdpdf} target="_blank" rel="noopener noreferrer" className="cv-link">
             Open CV (PDF)
           </a>
-            <img src={cvImage1} alt="CV Image 1" />
-            <img src={cvImage2} alt="CV Image 2" />
-          </div>
+          <img src={cvImage1} alt="CV Image 1" />
+          <img src={cvImage2} alt="CV Image 2" />
         </div>
       </div>
     </div>
